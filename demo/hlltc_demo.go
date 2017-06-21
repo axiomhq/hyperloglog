47e2f0ff66de2a416b01745eb259c25f189f77ae
Seif Lotfy <seif.lotfy@gmail.com>
2017-06-21 18:13:57 +0200

    Add mashalling to demo

--- demo/hlltc_demo.go
+++ demo/hlltc_demo.go
@@ -4,8 +4,7 @@ import (
 	"fmt"
 	"strconv"
 
-	"github.com/clarkduvall/hyperloglog"
-	metro "github.com/dgryski/go-metro"
+	hyperloglog "github.com/influxdata/influxdb/pkg/estimator/hll"
 	"github.com/seiflotfy/hlltc"
 )
 
@@ -31,10 +30,9 @@ func main() {
 	unique := map[string]bool{}
 
 	for i := 1; len(unique) <= 10000000; i++ {
-		str := strconv.Itoa(i)
+		str := "stream-" + strconv.Itoa(i)
 		hlltc.Insert([]byte(str))
-		item := fakeHash64(metro.Hash64([]byte(str), 1337))
-		hllpp.Add(item)
+		hllpp.Add([]byte(str))
 		unique[str] = true
 
 		if len(unique)%step == 0 || len(unique) == 10000000 {
@@ -44,7 +42,13 @@ func main() {
 			ratio := 100 * estimateError(res, exact)
 			res2 := uint64(hllpp.Count())
 			ratio2 := 100 * estimateError(res2, exact)
-			fmt.Printf("Exact %d, got:\n\thlltc %d (%.4f%% off)\n\thllpp %d (%.4f%% off)\n", exact, res, ratio, res2, ratio2)
+			fmt.Printf("Exact %d, got:\n\t axiom hlltc %d (%.4f%% off)\n\tinflux hllpp %d (%.4f%% off)\n", exact, res, ratio, res2, ratio2)
 		}
 	}
+
+	data1, _ := hllpp.MarshalBinary()
+	data2, _ := hlltc.MarshalBinary()
+	fmt.Println("AxiomHQ HLLTC total size", len(data2))
+	fmt.Println("InfluxData HLLPP total size", len(data1))
+
 }
