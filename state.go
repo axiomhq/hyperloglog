d82e80097276ae659c98a9208a53d0526564e0cc
Sean Marciniak <smarciniak@splunk.com>
2026-05-19 09:56:12 +0930

    Adding support for state representation

---
+++ state.go
@@ -0,0 +1,26 @@
+package hyperloglog
+
+// state allows for ease of tracking the current representation
+// of the sketch to allow for ease of restoring sketch state and reuse.
+// It is defined as type alias to for improved reuse throughout the codebase.
+type state byte
+
+const (
+	stateDense state = iota
+	stateSparse
+)
+
+func newState(sparse bool) state {
+	if sparse {
+		return stateSparse
+	}
+	return stateDense
+}
+
+func (s state) isSparse() bool {
+	return s == stateSparse
+}
+
+func (s state) Byte() byte {
+	return byte(s)
+}
